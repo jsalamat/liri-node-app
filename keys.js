c1fb887b2cae5692efd737c03845aa73147f7209
Jon Salamat <Jon Salamat>
2017-01-26 14:46:35 -0500

    twitter ok, spotify and omdbapi needs adjustment

--- keys.js
+++ keys.js
@@ -1,10 +1,11 @@
-console.log('this is loaded');
+// console.log('this is loaded');
+var Twitter = require('twitter');
 
-var twitteraccountkey = {
+var client = new Twitter({
   consumer_key: 'p7q42uVbU5n8ixGST96fxVg6h',
   consumer_secret: 'csDvM4n0EFhAR5d4v3RwibYhhgNIEyjQeYf0q3M6sxShudP6P0',
   access_token_key: '824403024688906241-D7MyvYojMXx99X52R0ETCjv4tuwyvIQ',
   access_token_secret: '2s13LSImaVP3inSAYWTwpRaAuiCJzyarXb5pAug8AuHZ0',
-}
+})
 
-exports.twitterKeys = twitteraccountkey
+exports.t = client
--- liri.js
+++ liri.js
@@ -1,51 +1,73 @@
-var fs = requiere("fs");
 
-var importtwitterkey = require('./keys.js');
+
+var importclient = require('./keys.js');
+var fs = require('fs');
 
 var request = require('request');
 var Twitter = require('twitter');
 var spotify = require('spotify');
 
-//twitter-----------------
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
-});
+var commands = process.argv[2];
+var titleName = process.argv[3];
 
+//twitter-----------------
+if (commands == "my-tweets"){
+	var params = {screen_name: 'SuperJWillS'};
+	importclient.t.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    // console.log(tweets);
+	    // console.log(tweets[0].text);
+	    for (var i = 0; i < tweets.length; i++){
+	    // for (var i = 0; i < tweets.length||i < 20; i++){
+	    	console.log(tweets[i].text);
+	    	console.log("");
+	    	console.log('-----------------------------------')
+	    }
+	  }
+	});
+}
 //end of twitter------------------------
 
 //spotify--------------------------------
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    } 
-    // Do something with 'data' 
-});
+if (commands == "spotify-this-song"){
+	spotify.search({ type: 'track', query: titleName }, function(err, data) {
+	    if ( err ) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    } 
+	    // Do something with 'data'
+	    	// console.log(data);
+	    	// console.log(data.tracks); 
+	    	// console.log(data.tracks.items);
+	    	console.log(data.tracks.items[2].artists.name);
+	    	// console.log(data.tracks.items[2].name); 
+	    	// console.log(data.tracks.items[2].artists); 
+	});
+}
 //end of spotify--------------------------------
 
 //Movie--------------------------------------
-// Grab or assemble the movie name and store it in a variable called "movieName"
-for (var i=2; i < process.argv.length; i++){
-    if (i == 2) movieName = movieName + process.argv[i];
-    else movieName = movieName + "+" + process.argv[i];
-    // "[space]" could work but we prefer "+" for omdapi
+if (commands == "movie-this"){
+	// Grab or assemble the movie name and store it in a variable called "titleName"
+	for (var i=4; i < process.argv.length; i++){
+	    if (i == 5) titleName = titleName + process.argv[i];
+	    else titleName = titleName + "+" + process.argv[i];
+	    // "[space]" could work but we prefer "+" for omdapi
+	}
+	console.log(titleName);
+
+	var queryUrl = "http://www.omdbapi.com/?t=" + titleName + "&y=&plot=short&r=json";
+	console.log(queryUrl);
+
+	request(queryUrl, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(JSON.parse(body));
+	    // If the request is successful
+	  // Then log the Release Year for the movie
+	    console.log(JSON.parse(body).Released);
+	      //JSON.parse()
+	    // the body is a string if it is  a json it is better to convert it to json
+	  }
+	})
 }
-console.log(movieName);
-
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
-console.log(queryUrl);
-
-request(queryUrl, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(JSON.parse(body));
-    // If the request is successful
-  // Then log the Release Year for the movie
-    console.log(JSON.parse(body).Released);
-      //JSON.parse()
-    // the body is a string if it is  a json it is better to convert it to json
-  }
-})
 //end of movie ----------------------------------
