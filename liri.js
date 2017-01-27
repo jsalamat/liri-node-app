f6b4142c5e4639064b0f2db4f19f73582bd6935e
Jon Salamat <Jon Salamat>
2017-01-27 01:11:19 -0500

    default added on spotify and movie, spotify fix

--- liri.js
+++ liri.js
@@ -19,6 +19,7 @@ if (commands == "my-tweets"){
 	    // console.log(tweets[0].text);
 	    for (var i = 0; i < tweets.length; i++){
 	    // for (var i = 0; i < tweets.length||i < 20; i++){
+	    	console.log("");
 	    	console.log(tweets[i].text);
 	    	console.log("");
 	    	console.log('------------------------------------------------------------------')
@@ -30,24 +31,40 @@ if (commands == "my-tweets"){
 
 //spotify--------------------------------
 if (commands == "spotify-this-song"){
+	if (!titleName){
+		titleName = "Ace of Base";
+	}
+	for (var i=4; i < process.argv.length; i++){
+	    if (i == 20) titleName = titleName + process.argv[i];
+	    // 20 is max word of the movie title, remember it start at 4
+	    else titleName = titleName + "+" + process.argv[i];
+	    // "[space]" could work but we prefer "+" for omdapi
+	}
 	spotify.search({ type: 'track', query: titleName }, function(err, data) {
 	    if ( err ) {
 	        console.log('Error occurred: ' + err);
 	        return;
 	    } 
-	    // Do something with 'data'
 	    	// console.log(data);
-	    	// console.log(data.tracks); 
-	    	// console.log(data.tracks.items);
-	    	console.log(data.tracks.items[2].artists.name);
-	    	// console.log(data.tracks.items[2].name); 
-	    	// console.log(data.tracks.items[2].artists); 
+	    	// console.log(JSON.stringify(data.tracks, null, 2)); 
+	    for (var i = 0; i < 20; i++){
+	    	console.log("");
+	    	console.log("Artist(s): " + data.tracks.items[i].artists[0].name);
+	    	console.log("Song\'s Name: " + data.tracks.items[i].name);
+	    	console.log("Preview Link: " + data.tracks.items[i].preview_url);
+	    	console.log("Album: " + data.tracks.items[i].album.name);
+	    	console.log("");
+	    	console.log('------------------------------------------------------------------')
+	    } 
 	});
 }
 //end of spotify--------------------------------
 
 //Movie--------------------------------------
 if (commands == "movie-this"){
+	if (!titleName){
+		titleName = "Mr Nobody";
+	}
 	// Grab or assemble the movie name and store it in a variable called "titleName"
 	for (var i=4; i < process.argv.length; i++){
 	    if (i == 20) titleName = titleName + process.argv[i];
@@ -63,7 +80,9 @@ if (commands == "movie-this"){
 	request(queryUrl, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	    // console.log(JSON.parse(body));
+
 	  	console.log('------------------------------------------------------------------')
+	  	console.log("");
 	  	console.log("Title: " + (JSON.parse(body).Title));
 	    console.log("Released: " + (JSON.parse(body).Released));
 	    console.log("");
@@ -76,6 +95,7 @@ if (commands == "movie-this"){
 	    console.log("");
 	    console.log("Rotten Tomatoes Rating: " + (JSON.parse(body).tomatoRating));
 	    console.log("Rotten Tomatoes URL: " + (JSON.parse(body).tomatoURL));
+	    console.log("");
 	    console.log('------------------------------------------------------------------')
 	  }
 	})
